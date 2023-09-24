5d88c914d3def470eac084c01f49a32520e4fb9a
User <User@WIN-E8F88FA2UKH>
2023-09-24 21:17:28 +0300

    Add project files.

---
+++ Clock/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Clock
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.cdShowCheck = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(204, 73);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "label1";
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// cdShowCheck
+			// 
+			this.cdShowCheck.AutoSize = true;
+			this.cdShowCheck.Location = new System.Drawing.Point(25, 111);
+			this.cdShowCheck.Name = "cdShowCheck";
+			this.cdShowCheck.Size = new System.Drawing.Size(79, 17);
+			this.cdShowCheck.TabIndex = 1;
+			this.cdShowCheck.Text = "Show Date";
+			this.cdShowCheck.UseVisualStyleBackColor = true;
+			this.cdShowCheck.CheckedChanged += new System.EventHandler(this.cdShowCheck_CheckedChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.BackgroundImage = global::Clock.Properties.Resources._1614598470_33_p_vremya_na_belom_fone_35;
+			this.ClientSize = new System.Drawing.Size(618, 737);
+			this.Controls.Add(this.cdShowCheck);
+			this.Controls.Add(this.label1);
+			this.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.TransparencyKey = System.Drawing.SystemColors.Control;
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.CheckBox cdShowCheck;
+	}
+}
+
---
+++ Clock/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace Clock
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			if (cdShowCheck.Checked == true)
+			{
+				label1.Text = DateTime.Now.ToString("HH:mm:ss");
+			}
+			else
+			{
+				label1.Text = DateTime.Now.ToString();
+			}
+		}
+
+		private void cdShowCheck_CheckedChanged(object sender, EventArgs e)
+		{
+			//if (cdShowCheck.Checked == true)
+			//{
+			//	label1.Text = DateTime.Now.ToString("HH:mm:ss");
+			//}
+			//else
+			//{
+			//	label1.Text = DateTime.Now.ToString("HH:ss:mm");
+			//}
+		}
+	}
+}
